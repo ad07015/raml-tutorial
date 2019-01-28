0c9b50dc79793c803ee2340dff996166dbd7007f
andrejs.dasko <andrejs.dasko@accenture.com>
2019-01-29 00:40:59 +0200

    Initial commit

---
+++ src/main/java/com/tutorial/raml/RamlApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.raml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamlApplication.class, args);
+	}
+
+}
+
